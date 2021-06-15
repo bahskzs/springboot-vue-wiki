a612ca462dee41030b96297f6c292d274e90c0ce
bahsk <bahskzs610@live.com>
2021-06-15 20:52:25 +0800

    2021-06-15 2-8 SpringBoot配置文件【自定义配置项】

---
+++ src/main/java/com/yqy/wiki/controller/HelloController.java
@@ -0,0 +1,24 @@
+package com.yqy.wiki.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author bahsk
+ * @createTime 2021-06-15 20:13
+ * @description
+ */
+
+@RestController
+public class HelloController {
+
+    //有则取demo,没有则取默认值DEMO
+    @Value("${demo:DEMO}")
+    private String demo;
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello" + demo;
+    }
+}
