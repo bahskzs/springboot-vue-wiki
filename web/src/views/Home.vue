3158637d563bcf61b1a5622af44e91f883bc6b62
bahsk <bahskzs610@live.com>
2021-06-21 23:20:08 +0800

    2021-06-21 4-5 集成Ant Design Vue 001 npm i --save ant-design-vue@next 002 main.ts import Antd from 'ant-design-vue'; import 'ant-design-vue/dist/antd.css'; app.use(Antd);
    xxx.vue  页面追加ant 组件页面的示例代码

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -3,6 +3,7 @@
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
+  <a-button type="primary">Primary</a-button>
 </template>
 
 <script lang="ts">
