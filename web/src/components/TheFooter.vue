6b3f849e4a21ca4176c2c5199324c748c109d5ce
bahsk <bahskzs610@live.com>
2021-06-22 22:59:44 +0800

    2021-06-22 4-7 1. 将header和footer封装成自定义组件 定义： <script lang="ts"> import { defineComponent } from 'vue';
    export default defineComponent({
      name: 'TheFooter',
    });
    </script>
    引用：
    import TheFooter from '@/components/TheFooter.vue';
    export default defineComponent({
      components: {
        ...
        TheHeader,
        TheFooter
    
      },
      ...
    });
    标签：<TheFooter></TheFooter>

---
+++ web/src/components/TheFooter.vue
@@ -0,0 +1,33 @@
+<template>
+  <a-layout-footer style="text-align: center">
+    Personal wiki ©2022 Created by bahskzs
+  </a-layout-footer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'TheFooter',
+});
+</script>
+
+<style>
+.logo {
+  float: left;
+  width: 120px;
+  height: 31px;
+  margin: 16px 24px 16px 0;
+  background: rgba(255, 255, 255, 0.3);
+}
+
+.ant-row-rtl #components-layout-demo-top-side-2 .logo {
+  float: right;
+  margin: 16px 0 16px 24px;
+}
+
+.site-layout-background {
+  background: #fff;
+}
+</style>
+
